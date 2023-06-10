c514d7c61079f8d4847955028a37cf41360ff799
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2023-06-10 17:46:09 +0100

    Signup Page UI now implemented (without backend)

--- E-Commerce-Site/E-Commerce-Site/ClientApp/src/forms/SignupForm.js
+++ E-Commerce-Site/E-Commerce-Site/ClientApp/src/forms/SignupForm.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SignupForm() {
-    
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(username, password);
+    };
+
     return (
-        <form className="signup-form">
-            <p> The Signup Page</p>
+        <form className="form" onSubmit={handleSubmit}>
+            <h1>Create Account</h1>
+            <label> Username: </label>
+            <input
+                type="text"
+                className="form-field"
+                placeholder="Username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+                required
+            />
+
+            <label> Password: </label>
+            <input
+                type="password"
+                className="form-field"
+                placeholder="Password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                required
+            />
+
+            <button type="submit" className="my-3 rounded-pill purchase-link d-block d-md-none">Register</button>
         </form>
     );
 }
