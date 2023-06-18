8a020ab6318b264cca748e69789998f97db83f9e
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2023-06-18 14:06:27 +0100

    Username now gets displayed when you hover of the user icon

--- E-Commerce-Site/E-Commerce-Site/ClientApp/src/components/NavMenu.js
+++ E-Commerce-Site/E-Commerce-Site/ClientApp/src/components/NavMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container, Navbar, NavbarBrand, Nav } from 'reactstrap';
 import {useHistory} from 'react-router-dom';
 import './NavMenu.css';
@@ -6,23 +6,41 @@ import {faUser} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const NavMenu = () => {
+  // Example username until user login implemented
+  const [username, setUsername] = useState('Evie');
+  const [showUsername, setShowUsername] = useState(false);
+  
   const history = useHistory();
 
   const handleIconClick = () => {
     history.push('/login');
   };
 
+  const handleIconHover = () => {
+    setShowUsername(true);
+  };
 
+  const handleIconLeave = () => {
+    setShowUsername(false);
+  };
+  
   return (
       <header>
         <Navbar color="dark" dark expand="md">
           <Container>
             <NavbarBrand href="/">Shop</NavbarBrand>
             <Nav className="ml-auto" navbar>
+              {showUsername && (
+                  <div className="username">
+                    {username}
+                  </div>
+              )}
               <FontAwesomeIcon
                   icon={faUser}
-                  style={{color: 'white', cursor: 'pointer' }}
+                  className="user-icon"
                   onClick={handleIconClick}
+                  onMouseEnter={handleIconHover}
+                  onMouseLeave={handleIconLeave}
               />
             </Nav>
           </Container>
