a67edb26e1f2ef87842f50896d9c16c7d0f34e49
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2024-01-01 13:18:37 +0000

    Reduced SQL injection risk in the Cart services

--- Python_BackEnd/Cart/CartService.py
+++ Python_BackEnd/Cart/CartService.py
@@ -3,30 +3,39 @@ from Cart.Models.Cart import CartItem, CartProductCombo, QuantityClass
 
 
 async def add_item_to_cart(cart: CartItem):
-    command = f"EXEC dbo.addItemToCart @cartId='{cart.cartId}', @userId='{cart.userId}', @productId='{cart.productId}', @quantity='{cart.quantity}'"
+    command = "EXEC dbo.addItemToCart @cartId=?, @userId=?, @productId=?, @quantity=?"
+    params = (
+        cart.cartId,
+        cart.userId,
+        cart.productId,
+        cart.quantity
+    )
     db = get_db_connection()
     with db.cursor() as cursor:
-        cursor.execute(command)
+        cursor.execute(command, params)
 
 
 async def get_cart_items(user_id: str):
-    command = f"EXEC dbo.get_cart_items @userId='{user_id}'"
+    command = f"EXEC dbo.get_cart_items @userId=?"
+    params = (user_id,)
     db = get_db_connection()
     with db.cursor() as cursor:
-        cursor.execute(command)
+        cursor.execute(command, params)
         results = [CartProductCombo(*row) for row in cursor]
     return results
 
 
 async def update_quantity(cart: QuantityClass):
-    command = f"EXEC dbo.alterQuantity @cartId='{cart.cartId}', @quantity_change='{cart.quantityChange}'"
+    command = f"EXEC dbo.alterQuantity @cartId=?, @quantity_change=?"
+    params = (cart.cartId, cart.quantityChange)
     db = get_db_connection()
     with db.cursor() as cursor:
-        cursor.execute(command)
+        cursor.execute(command, params)
 
 
 async def remove_item(cart_id: str):
-    command = f"EXEC dbo.remove_item @cartId='{cart_id}'"
+    command = f"EXEC dbo.remove_item @cartId=?"
+    params = (cart_id,)
     db = get_db_connection()
     with db.cursor() as cursor:
-        cursor.execute(command)
+        cursor.execute(command, params)
