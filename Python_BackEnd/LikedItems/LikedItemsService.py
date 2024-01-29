650329d6b6f881ad4b9406176e1d26af6a921c5a
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2024-01-29 22:45:41 +0000

    Implemented back-end for get_liked_items

--- Python_BackEnd/LikedItems/LikedItemsService.py
+++ Python_BackEnd/LikedItems/LikedItemsService.py
@@ -1,6 +1,7 @@
 from database import get_db_connection
 from LikedItems.Models.LikedItems import LikedItem
 import uuid
+from Products.Models.Products import Products
 
 
 async def add_liked_item(liked_items: LikedItem) -> None:
@@ -16,3 +17,18 @@ async def add_liked_item(liked_items: LikedItem) -> None:
     db = get_db_connection()
     with db.cursor() as cursor:
         cursor.execute(command, params)
+
+
+async def get_liked_items(user_id: str) -> list[Products]:
+    """
+        Gets all liked items for a given userID
+        Returns: list[Products] A list of all products the user has liked
+   """
+    command = "EXEC dbo.get_liked_items @userId=?"
+    params = user_id
+    db = get_db_connection()
+    with db.cursor() as cursor:
+        cursor.execute(command, params)
+        results = [Products(id=uuid.UUID(row[0]), title=row[1], imagePath=row[2],
+                            retailer=row[3], cost=row[4]) for row in cursor]
+    return results
