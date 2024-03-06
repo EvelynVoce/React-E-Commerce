c7b20822160e6b9083743f05185783aef14db21e
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2024-03-06 17:33:15 +0000

    Cart Typescript (with model directory)

---
+++ E-Commerce-Site/E-Commerce-Site/ClientApp/src/api/cart.tsx
@@ -0,0 +1,23 @@
+﻿// @ts-ignore
+import { client } from "./http-helpers";
+import cart from "../Models/Cart";
+
+export const addItemToCart = (userID: string, productID: string, quantity: number) => {
+    return client.post('addItemToCart', { userID, productID, quantity }).then((response: { data: null; }) => response.data);
+}
+
+export const getCartItems = (userID: string) => {
+    return client.get(`getCartItems/${userID}`).then((response: { data: cart[]; }) => response.data);
+}
+
+/*export const getProductsInCart = (productIdsInCart) => {
+    return client.get(`getProductsInCart/${productIdsInCart}`).then(response => response.data);
+}*/
+
+export const updateStoredQuantity = (cartId: string, quantityChange: number) => {
+    return client.post('updateQuantity', { cartId, quantityChange }).then((response: { data: null; }) => response.data);
+}
+
+export const removeItem = (cartId: string) => {
+    return client.post('removeItem', { cartId }).then((response: { data: null; }) => response.data);
+}
