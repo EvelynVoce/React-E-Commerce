c04638e2d1924357be54d9f7dbe8e70c91935c0f
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2024-03-06 15:15:20 +0000

    index and home page updated

--- E-Commerce-Site/E-Commerce-Site/ClientApp/src/index.tsx
+++ E-Commerce-Site/E-Commerce-Site/ClientApp/src/index.tsx
@@ -2,7 +2,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+// @ts-ignore
 import App from './App';
+// @ts-ignore
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons';
@@ -10,16 +12,15 @@ import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
 
 library.add(fasHeart, farHeart);
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+// Ensure baseUrl is a string or undefined
+const baseUrl = document.getElementsByTagName('base')[0]?.getAttribute('href') || '/';
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
+    <BrowserRouter basename={baseUrl}>
+        <App/>
+    </BrowserRouter>,
+rootElement
+);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
