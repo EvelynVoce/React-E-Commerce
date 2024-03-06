a984cfe0437be4f6b6852e017b0f1b9e9cfda4ca
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2024-03-06 15:41:58 +0000

    Updated CartCards to use typescript

--- E-Commerce-Site/E-Commerce-Site/ClientApp/src/components/CartCards.tsx
+++ E-Commerce-Site/E-Commerce-Site/ClientApp/src/components/CartCards.tsx
@@ -1,14 +1,25 @@
 ﻿import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+// @ts-ignore
 import {removeItem, updateStoredQuantity} from "../api/cart";
 import Modal from "react-modal";
 
-const Card = ({triggerEffect, cartId, title, initial_quantity, imagePath, retailer, cost }) => {
+interface CardProps {
+    triggerEffect: () => void;
+    cartId: string;
+    title: string;
+    initial_quantity: number;
+    imagePath: string;
+    retailer: string;
+    cost: number;
+}
+
+const Card: React.FC<CardProps> = ({ triggerEffect, cartId, title, initial_quantity, imagePath, retailer, cost }) => {
     const [quantity, setQuantity] = useState(initial_quantity);
     const [showModal, setShowModal] = useState(false);
 
-    const appElement = document.getElementById('root'); // or any other root element of your React app
+    const appElement: HTMLElement | null = document.getElementById('root');
 
 
     const incrementCount = async () => {
@@ -51,29 +62,32 @@ const Card = ({triggerEffect, cartId, title, initial_quantity, imagePath, retail
                     <FontAwesomeIcon className="quantity-button" onClick={incrementCount} icon={faPlus}/>
                 </div>
 
-                <Modal
-                    appElement={appElement}
-                    isOpen={showModal}
-                    onRequestClose={handleCancel}
-                    className="modal_cart"
-                    contentLabel="Remove Item Modal"
-                >
-                    <p>Are you sure you want to remove this item from your cart?</p>
-                    <div className="button-container">
-                        <button className="mx-2 rounded-pill purchase-link" onClick={handleRemoveItem}>Yes</button>
-                        <button className="mx-2 rounded-pill purchase-link" onClick={handleCancel}>No</button>
-                    </div>
-                </Modal>
+                {(appElement != null ) ? (
+                    <Modal
+                        appElement={appElement}
+                        isOpen={showModal}
+                        onRequestClose={handleCancel}
+                        className="modal_cart"
+                        contentLabel="Remove Item Modal"
+                    >
+                        <p>Are you sure you want to remove this item from your cart?</p>
+                        <div className="button-container">
+                            <button className="mx-2 rounded-pill purchase-link" onClick={handleRemoveItem}>Yes</button>
+                            <button className="mx-2 rounded-pill purchase-link" onClick={handleCancel}>No</button>
+                        </div>
+                    </Modal>
+                ): ""}
             </div>
         </div>
     );
 };
 
+// @ts-ignore
 const CartCards = ({ triggerEffect, data }) => {
     return (
         <div className="container mt-5">
             <div id="cart-items" className="mt-4">
-                {data.map((item, index) => (
+                {data.map((item: { cartId: any; quantity: any; title: any; imagePath: any; retailer: any; cost: any; }, index: React.Key | null | undefined) => (
                     <Card key={index} triggerEffect={triggerEffect} cartId={item.cartId} initial_quantity={item.quantity} title={item.title} imagePath={item.imagePath}
                           retailer={item.retailer} cost={item.cost} />
                 ))}
